1d53d13a467e48473d1486b7aeff393e10fc5470
astarta16 <ngedeshuri2001@gmail.com>
2023-04-03 01:53:30 +0400

    add log in modal

--- static/js/modal.js
+++ static/js/modal.js
@@ -1,17 +1,19 @@
+// Get the modal
 const modal = document.getElementById("myModal");
-const btn = document.getElementById("myBtn");
+
+// Get the button that opens the modal
+const btn = document.getElementById("openModalBtn");
+
+// Get the <span> element that closes the modal
 const span = document.getElementsByClassName("close")[0];
 
+// When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
+// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
